02342e1030a2012adb83e50ff06a77584f789d74
SilvenLEAF <SilvenLEAF@gmail.com>
2021-11-07 14:12:57 +0530

    when all tests pass

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -7,9 +7,9 @@ describe('Add function Tests', () => {
     expect(add(5, 3)).toBe(8)
   })
 
-  test('Adding 7 and 2 to be string "9"', () => {
-    expect(add(7, 2)).toBe('9')
-  })
+  // test('Adding 7 and 2 to be string "9"', () => {
+  //   expect(add(7, 2)).toBe('9')
+  // })
   
   test('Adding 3 and 2 to be 5', () => {
     expect(add(3, 2)).toBe(5)
